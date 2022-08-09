b6ef50ee65a2ff6e0679f024ac6ad7e6de77fa6c
drpride <2146343874@qq.com>
2022-08-09 18:31:04 +0800

    add and implenment pdf search method && implenment extractor.py

--- extractor.py
+++ extractor.py
@@ -6,26 +6,51 @@
 # @Describe:
 import os
 import zipfile
+import shutil
 import py7zr
 import rarfile
 
 def extract(file_path):
     # 返回一个文本文件夹路径和图片文件夹路径
-
     # 获取文件后缀名，判断类型执行相应的查找方法
     file_type = file_path[file_path.rfind('.'):]
     # office类型的文件均可以通过zip解压的方式来分离图片和文本
-    office_type = ['.docx', '.doc', '.ppt', '.pptx', '.xlsx']
-    compressed_file_type = ['.zip', '.7z', '.rar']
+    office_type = ['.docx', '.doc', '.ppt', '.pptx', '.xlsx', '.zip']
+    compressed_file_type = ['.7z', '.rar']
+    img_type_list = ['.jpg', '.png', 'jpeg']
+    text_info_type_list = ['.txt', '.xml']
     if file_type in office_type:
-        txt_path,img_path = extract_office_file(file_path)
-    elif file_path in compressed_file_type:
-        txt_path,img_path = extract_compressed_file(file_path)
+        root_path =  file_path[:file_path.rfind('.')]
+        img_path = root_path + '/img'
+        xml_path = root_path + '/xml'
+        with zipfile.ZipFile(file_path) as f:
+            for file in f.namelist():
+                file_type = file[file.rfind('.'):]
+                if file_type in img_type_list:
+                    f.extract(file, path=img_path)
+                elif file_type == '.xml':
+                    f.extract(file, path=xml_path)
+    elif file_type in compressed_file_type:
+        root_path =  file_path[:file_path.rfind('.')]
+        if file_type == '.rar':
+            img_path = root_path + '/img'
+            text_info_path = root_path + '/text_info'
+            with rarfile.RarFile(file_path) as f:
+                for file in f.namelist():
+                    file_type = file[file.rfind('.'):]
+                    if file_type in img_type_list:
+                        f.extract(file, path=img_path)
+                    elif file_type in text_info_type_list:
+                        f.extract(file, path=text_info_path)
+        elif file_type == '.7z':
+            img_path = root_path + '/img'
+            text_info_path = root_path + '/text_info'
+            with py7zr.SevenZipFile(file_path) as f:
+                for file in f.getnames():
+                    file_type = file[file.rfind('.'):]
+                    if file_type in img_type_list:
+                        f.extract(file, path=img_path)
+                    elif file_type in text_info_type_list:
+                        f.extract(file, path=text_info_path)
+    return img_path, xml_path
 
-def extract_office_file(file_path):
-    # 解压文本文件和图片文件到指定文件夹 并返回路径
-    pass
-
-def extract_compressed_file(file_path):
-    # 解压文本文件和图片文件到指定文件夹 并返回路径
-    pass
